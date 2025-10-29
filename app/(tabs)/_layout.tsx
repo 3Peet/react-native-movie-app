614ac8b9719a317f38f5cd85793b4f56831d4a22
3Peet <mypeet096@gmail.com>
2025-10-29 22:30:33 +0700

    feat: WIP navbar

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,24 @@
+import { Tabs } from "expo-router";
+
+export default function Layout() {
+	return (
+		<Tabs>
+			<Tabs.Screen
+				name="index"
+				options={{ title: "home", headerShown: false }}
+			/>
+			<Tabs.Screen
+				name="search"
+				options={{ title: "Search", headerShown: false }}
+			/>
+			<Tabs.Screen
+				name="saved"
+				options={{ title: "Saved", headerShown: false }}
+			/>
+			<Tabs.Screen
+				name="profile"
+				options={{ title: "Profile", headerShown: false }}
+			/>
+		</Tabs>
+	);
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Home() {
 	return (
 		<View className="flex-1 items-center justify-center">
-			<Text className="text-5xl text-blue-500">Welcome!</Text>
+			<Text className="font-bold text-5xl text-violet-500">Welcome!</Text>
 		</View>
 	);
 }
